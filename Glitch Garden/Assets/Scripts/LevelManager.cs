abfc8034d696fd1356ec9ea61423ebb41d78a0bb
wcalvert88 <wcalvert88@gmail.com>
2018-11-02 20:28:13 -0400

    created a splash screen to start menu

---
+++ Glitch Garden/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LevelManager : MonoBehaviour {
+
+	[SerializeField] float endSplash = 2.5f;
+
+	void Start() {
+		Invoke("LoadNextLevel", endSplash);
+	}
+	public void LoadLevel(string name) {
+		Debug.Log("New Level load: " + name);
+		SceneManager.LoadScene(name);
+	}
+	
+	public void QuitRequest() {
+		Debug.Log("Quit requested");
+		Application.Quit();
+	}
+
+	public void LoadNextLevel() {
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+	
+}
