9424eade93ec857046307eddb096234510c2cce5
wcalvert88 <wcalvert88@gmail.com>
2018-11-29 08:24:09 -0500

    made the attackers mobile through scripting

---
+++ Glitch Garden/Assets/Scripts/Attacker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacker : MonoBehaviour {
+
+	[SerializeField] [Range (-1f, 1.5f)] float walkSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.left * walkSpeed * Time.deltaTime);
+	}
+}
