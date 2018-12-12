e1d5e73032c6ffcf4352ee6ec4aca995bcc53c2f
Wade Calvert <wcalvert88@gmail.com>
2018-12-12 15:01:24 -0500

    made buttons work and can place defenders anywhere on the grid

---
+++ Glitch Garden/Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderSpawner : MonoBehaviour {
+
+	[SerializeField] public Camera myCamera;
+	[SerializeField] private GameObject defenderParent;
+
+	void Start() {
+		defenderParent = GameObject.Find("Defenders");
+
+		if (!defenderParent) {
+			defenderParent = new GameObject("Defenders");
+		}
+	}
+
+	void OnMouseDown()
+	{
+		Vector2 rawPos = CalculateWorldPointOfMouseClick();
+		Vector2 roundedPos = SnapToGrid(rawPos);
+		GameObject defender = Button.selectedDefender;
+		Quaternion zeroRotation = Quaternion.identity;
+		GameObject newDef = Instantiate(defender, roundedPos, zeroRotation) as GameObject;
+
+		newDef.transform.parent = defenderParent.transform;
+	}
+
+	Vector2 SnapToGrid(Vector2 rawWorldPos) {
+		float newX = Mathf.RoundToInt(rawWorldPos.x);
+		float newY = Mathf.RoundToInt(rawWorldPos.y);
+		return new Vector2(newX, newY);
+	}
+
+	Vector2 CalculateWorldPointOfMouseClick() {
+		float mouseX = Input.mousePosition.x;
+		float mouseY = Input.mousePosition.y;
+		float distanceFromCamera = 10f;
+		Vector3 weirdTriplet = new Vector3(mouseX, mouseY, distanceFromCamera);
+		Vector2 worldPos = myCamera.ScreenToWorldPoint(weirdTriplet);
+
+		return worldPos;
+	}
+}
