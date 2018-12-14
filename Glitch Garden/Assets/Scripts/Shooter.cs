7583a1b144b4000b2a19678cd1d8cf7f25ddebe8
Wade Calvert <wcalvert88@gmail.com>
2018-12-14 08:53:55 -0500

    updated shooted.cs to trigger defenders to shoot only if there is an enemy in it's lane

--- Glitch Garden/Assets/Scripts/Shooter.cs
+++ Glitch Garden/Assets/Scripts/Shooter.cs
@@ -6,14 +6,62 @@ public class Shooter : MonoBehaviour {
 
 	[SerializeField] public GameObject projectile, gun;
 	[SerializeField] private GameObject projectileParent;
+	private Animator animator;
+	private Spawner myLaneSpawner;
+	
 
 	void Start() {
+		animator = GetComponent<Animator>();
+
+		// Creates a parent if necessary
 		projectileParent = GameObject.Find("Projectiles");
 
 		if (!projectileParent) {
 			projectileParent = new GameObject("Projectiles");
 		}
+
+		SetMyLaneSpawner();
 	}
+
+	void Update() {
+		animator.SetBool("isAttacking", IsAttackerAheadInLane());
+		// if (IsAttackerAheadInLane()) {
+		// 	animator.SetBool("isAttacking", true);
+		// } else {
+		// 	animator.SetBool("isAttacking", false);
+		// }
+	}
+
+	bool IsAttackerAheadInLane() {
+		// Exit if no attackers in lane
+		if (myLaneSpawner.transform.childCount < 1) {
+			return false;
+		}
+		foreach (Transform attacker in myLaneSpawner.transform) {
+			if (attacker.transform.position.x > transform.position.x) {
+				return true;
+			}
+		}
+		// Attackers in lane, but behind us.
+		return false;
+	}
+
+	// Look through all spawners, and set myLaneSpawner if found
+	void SetMyLaneSpawner() {
+		Spawner[] spawnerArray = GameObject.FindObjectsOfType<Spawner>();
+
+		foreach (Spawner spawner in spawnerArray)
+		{
+			if (spawner.transform.position.y == transform.position.y) {
+				myLaneSpawner = spawner;
+				return;
+			}
+		}
+		if (!myLaneSpawner) {
+			Debug.LogError(name + " can't find spawner in lane!");
+		}
+	}
+
 	private void Fire() {
 		GameObject newProjectile = Instantiate(projectile) as GameObject;
 		newProjectile.transform.parent = projectileParent.transform;
