553f8c262b2d31a947ca99b9332e6e6d40bc30c6
Wade Calvert <wcalvert88@gmail.com>
2018-12-17 15:20:16 -0500

    created a win condition

---
+++ Glitch Garden/Assets/Scripts/GameTimer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour {
+
+	[SerializeField] public float levelSeconds = 100.0f;
+	[SerializeField] private AudioSource audioSource;
+	[SerializeField] private Slider slider;
+	[SerializeField] private bool isEndOfLevel = false;
+	[SerializeField] private LevelManager levelManager;
+	[SerializeField] private GameObject winLabel;
+
+
+	private float waitTime;
+
+	// Use this for initialization
+	void Start () {
+		slider = GetComponent<Slider>();
+		audioSource = GetComponent<AudioSource>();
+		levelManager = GameObject.FindObjectOfType<LevelManager>();
+		FindYouWin();
+		winLabel.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		slider.value = Time.timeSinceLevelLoad / levelSeconds;
+		bool timeIsUp = (Time.timeSinceLevelLoad >= levelSeconds);
+		if (timeIsUp && !isEndOfLevel) {
+			winLabel.SetActive(true);
+			audioSource.Play();
+			Invoke("LoadNextLevel", audioSource.clip.length);
+			isEndOfLevel = true;
+		}
+	}
+
+	void FindYouWin() {
+		winLabel = GameObject.Find("You Win");
+		if (!winLabel) {
+			Debug.LogWarning("Please create You Win Object");
+		}
+	}
+
+	void LoadNextLevel() {
+		levelManager.LoadNextLevel();
+	}
+}
