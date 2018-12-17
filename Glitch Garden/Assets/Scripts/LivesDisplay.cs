4e99242adef6c88182da8d81d5206190d26ea58f
Wade Calvert <wcalvert88@gmail.com>
2018-12-17 10:59:24 -0500

    created a lose collider and a lives tracker

---
+++ Glitch Garden/Assets/Scripts/LivesDisplay.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class LivesDisplay : MonoBehaviour {
+
+	[SerializeField] private Text liveText;
+	[SerializeField] int lives = 3;
+	public enum Status {SUCCESS, FAILURE};
+
+	void Start() {
+		liveText = GetComponent<Text>();
+		// lives = 100;
+		UpdateDisplay();
+	}
+
+	public void AddLives(int amount) {
+		lives += amount;
+		UpdateDisplay();
+	}
+
+	public Status UseLives(int amount) {
+		if (lives - amount >= 0) {
+			lives -= amount;
+			UpdateDisplay();
+			return Status.SUCCESS;
+		}
+		return Status.FAILURE;
+	}
+
+	private void UpdateDisplay() {
+		liveText.text = lives.ToString();
+	}
+}
---
+++ Glitch Garden/Assets/Scripts/Lose.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Lose : MonoBehaviour {
+	[SerializeField] private LivesDisplay livesDisplay;
+	[SerializeField] private LevelManager levelManager;
+
+	void Start() {
+		livesDisplay = GameObject.FindObjectOfType<LivesDisplay>();
+		levelManager = GameObject.FindObjectOfType<LevelManager>();
+	}
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		Destroy(other.gameObject);
+		if (livesDisplay.UseLives(1) == LivesDisplay.Status.FAILURE) {
+			levelManager.LoadLevel("03b_Lose");
+		} else {
+			livesDisplay.UseLives(1);
+		}
+	}
+}
